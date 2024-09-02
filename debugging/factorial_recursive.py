01b9cb6d9bd5c1b415e30b57f4727f8c471ba959
diarra192 <dboureima192@gmail.com>
2024-09-02 21:54:32 +0200

    Correction de sfichiers

---
+++ debugging/factorial_recursive.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    """
+    Calcule le factoriel d'un nombre entier donné.
+
+    Parameters:
+    n (int): Le nombre entier pour lequel calculer le factoriel. 
+             Doit être un entier non négatif.
+
+    Returns:
+    int: Le factoriel du nombre donné. Si n est 0, retourne 1 car 0! = 1.
+    """
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+# Calcul du factoriel du nombre passé en argument depuis la ligne de commande
+f = factorial(int(sys.argv[1]))
+print(f)
+
